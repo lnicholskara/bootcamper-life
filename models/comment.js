7f3efda3315620cd1f53cdba1f81b7cd9260b458
Kara Nichols <lnicholskara@gmail.com>
2019-03-23 11:56:49 -0700

    Sequelize naming fixes to match passport

--- models/comment.js
+++ models/comment.js
@@ -1,20 +1,26 @@
 module.exports = function(sequelize, DataTypes) {
   //Sequelize - MySQL table setup
-  var Comment = sequelize.define("Comment", {
-    body: {
-      type: DataTypes.TEXT,
-      allowNull: false
+  var Comment = sequelize.define(
+    "Comment",
+    {
+      body: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      },
+      active: { type: DataTypes.BOOLEAN, defaultValue: true },
+      votes: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+        validate: { isInt: true }
+      }
     },
-    active: { type: DataTypes.BOOLEAN, defaultValue: true },
-    votes: {
-      type: DataTypes.INTEGER,
-      defaultValue: 0,
-      validate: { isInt: true }
+    {
+      freezeTableName: true
     }
-  });
+  );
   //Associate Post to Profile
   Comment.associate = function(models) {
-    Comment.belongsTo(models.Profile, {
+    Comment.belongsTo(models.Users, {
       foreignKey: {
         allowNull: false
       }
--- models/post.js
+++ models/post.js
@@ -1,34 +1,40 @@
 module.exports = function(sequelize, DataTypes) {
   //Sequelize - MySQL table setup
-  var Post = sequelize.define("Post", {
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1, 100]
+  var Post = sequelize.define(
+    "Post",
+    {
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1, 100]
+        }
+      },
+      category: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1, 40]
+        }
+      },
+      body: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      },
+      active: { type: DataTypes.BOOLEAN, defaultValue: true },
+      votes: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+        validate: { isInt: true }
       }
     },
-    category: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1, 40]
-      }
-    },
-    body: {
-      type: DataTypes.TEXT,
-      allowNull: false
-    },
-    active: { type: DataTypes.BOOLEAN, defaultValue: true },
-    votes: {
-      type: DataTypes.INTEGER,
-      defaultValue: 0,
-      validate: { isInt: true }
+    {
+      freezeTableName: true
     }
-  });
+  );
   //Associate Post to Profile
   Post.associate = function(models) {
-    Post.belongsTo(models.Profile, {
+    Post.belongsTo(models.Users, {
       foreignKey: {
         allowNull: false
       }
