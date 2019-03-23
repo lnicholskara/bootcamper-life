6e777e41b6e8d5951a4c5d793fb12dd3d1f359e2
Kara Nichols <lnicholskara@gmail.com>
2019-03-23 11:44:04 -0700

    Update schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,16 +1,2 @@
 DROP DATABASE IF EXISTS bootcamp_db;
 CREATE DATABASE bootcamp_db;
-
-USE bootcamp_db;
-CREATE TABLE users
-(
-	id int NOT NULL AUTO_INCREMENT,
-	email varchar(255) NOT NULL,
-    password_hash varchar(255) NOT NULL,
-	first_name varchar(255) NOT NULL,
-    last_name varchar(255) NOT NULL,
-	campus varchar(255) NOT NULL,
-	city varchar(255) NOT NULL,
-	github_link varchar(255) NOT NULL,
-	PRIMARY KEY (id)
-);
