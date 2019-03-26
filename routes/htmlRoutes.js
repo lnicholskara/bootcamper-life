f0e32e6d05df3e85cdd95bf08997310d3bddceb1
Dendevpro <mauro.facciolla@gmail.com>
2019-03-26 02:09:10 -0700

    Added /reset route. Still needs authentication

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,21 +1,27 @@
+/* eslint-disable prettier/prettier */
 var db = require("../models");
 
 // var authController = require("../controllers/authcontroller.js");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Default Code - Load index page
-  app.get("/", function(req, res) {
+  app.get("/", function (req, res) {
     res.render("index", {});
   });
 
+  // "/reset" - Page where user resets the password
+  app.get("/reset", function (req, res) {
+    res.render("reset", {});
+  });
+
   // "/profile" - Page where user lands after login
-  app.get("/profile", isLoggedIn, function(req, res) {
+  app.get("/profile", isLoggedIn, function (req, res) {
     res.render("profile", {
       user: req.user
     });
   });
 
-  app.get("/logout", function(req, res) {
+  app.get("/logout", function (req, res) {
     req.logout();
     res.redirect("/");
   });
@@ -30,7 +36,7 @@ module.exports = function(app) {
   // "/updateprofile" - Page with form to update existing profile
   // "/network" - Load table of user profiles
   // "/posts" - Load main page with a table of posts
-  app.get("/posts", function(req, res) {
+  app.get("/posts", function (req, res) {
     res.render("posts", {});
   });
   // "/createpost" - Page with form to create new post
@@ -39,8 +45,8 @@ module.exports = function(app) {
   //************************************************/
   // Load single profile by id
 
-  app.get("/profile/:id", function(req, res) {
-    db.Profile.findOne({ where: { id: req.params.id } }).then(function(
+  app.get("/profile/:id", function (req, res) {
+    db.Profile.findOne({ where: { id: req.params.id } }).then(function (
       oneProfile
     ) {
       res.render("singleProfile", {
@@ -50,11 +56,11 @@ module.exports = function(app) {
   });
 
   // Load single post by id
-  app.get("/posts/:id", function(req, res) {
+  app.get("/posts/:id", function (req, res) {
     db.Profile.findOne({
       where: { id: req.params.id },
       include: [db.Profile]
-    }).then(function(onePost) {
+    }).then(function (onePost) {
       res.render("singleProfile", {
         post: onePost
       });
@@ -64,7 +70,7 @@ module.exports = function(app) {
   //************************************************/
   // Render 404 page for any unmatched routes
 
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.render("404");
   });
 };
