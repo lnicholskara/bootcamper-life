db0545a42322a7642b0e5d1411e47a81a11ad0e2
Dendevpro <mauro.facciolla@gmail.com>
2019-03-26 02:12:43 -0700

    Replaced the animated Title

--- public/js/app.js
+++ public/js/app.js
@@ -1,48 +1,56 @@
+/* eslint-disable no-unused-vars */
 // ********************************************* //
 // ***************  DO NOT TOUCH *************** //
 // ********************************************* //
 
 // TYPEWRITER FOR H3 //
-document.addEventListener('DOMContentLoaded',function(event){
+// eslint-disable-next-line prettier/prettier
+document.addEventListener("DOMContentLoaded", function (event) {
   // array with texts to type in typewriter
-  var dataText = [ 
-    "Together we strive", 
-    "Together we build better", 
-    "Together we make dreams come true!"];
-  
+  var dataText = [
+    "Together we build better",
+    "Together we build faster",
+    "Together we make dreams come true"
+  ];
+
   // type one text in the typwriter
   // keeps calling itself until the text is finished
   function typeWriter(text, i, fnCallback) {
     // chekc if text isn't finished yet
-    if (i < (text.length)) {
+    if (i < text.length) {
       // add next character to h1
-     document.querySelector("h2").innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>';
+      document.querySelector("#cta-title").innerHTML =
+        // eslint-disable-next-line prettier/prettier
+        text.substring(0, i + 1) + "<span aria-hidden=\"true\"></span>";
 
       // wait for a while and call this function again for next character
-      setTimeout(function() {
-        typeWriter(text, i + 1, fnCallback)
+      // eslint-disable-next-line prettier/prettier
+      setTimeout(function () {
+        typeWriter(text, i + 1, fnCallback);
       }, 100);
     }
     // text finished, call callback if there is a callback function
-    else if (typeof fnCallback == 'function') {
+    else if (typeof fnCallback === "function") {
       // call callback after timeout
       setTimeout(fnCallback, 700);
     }
   }
   // start a typewriter animation for a text in the dataText array
-   function StartTextAnimation(i) {
-     if (typeof dataText[i] == 'undefined'){
-        setTimeout(function() {
-          StartTextAnimation(0);
-        }, 20000);
-     }
-     // check if dataText[i] exists
+  function StartTextAnimation(i) {
+    if (typeof dataText[i] === "undefined") {
+      // eslint-disable-next-line prettier/prettier
+      setTimeout(function () {
+        StartTextAnimation(0);
+      }, 20000);
+    }
+    // check if dataText[i] exists
     if (i < dataText.length) {
       // text exists! start typewriter animation
-     typeWriter(dataText[i], 0, function(){
-       // after callback (and whole text has been animated), start next text
-       StartTextAnimation(i + 1);
-     });
+      // eslint-disable-next-line prettier/prettier
+      typeWriter(dataText[i], 0, function () {
+        // after callback (and whole text has been animated), start next text
+        StartTextAnimation(i + 1);
+      });
     }
   }
   // start the text animation
@@ -153,4 +161,3 @@ document.addEventListener('DOMContentLoaded',function(event){
 // // Add event listeners to the submit and delete buttons
 // $submitBtn.on("click", handleFormSubmit);
 // $exampleList.on("click", ".delete", handleDeleteBtnClick);
-
