11a964ba3ce9d70fe5bfac0e1a3b6b5ed86cd20b
Kara Nichols <lnicholskara@gmail.com>
2019-03-26 21:46:29 -0700

    create new post logic working

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ require("./config/passport")(passport, db.User);
 // var test = require('./routes/htmlRoutes');
 // test(app);
 
-var syncOptions = { force: false };
+var syncOptions = { force: true };
 
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
